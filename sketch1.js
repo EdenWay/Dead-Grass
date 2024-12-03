ac0b0ae4b58d1f43da1a85d0803b08a7ef421b7b
RoguishDelight <calkroth@gmail.com>
2024-12-03 12:52:06 -0600

    Update sketch1.js

--- sketch1.js
+++ sketch1.js
@@ -1,1 +1,17 @@
+let song1; 
+let isPlaying=false;
 
+function preload(){
+   // img = loadImage("");
+}
+
+function setup(){
+    createCanvas(800,400);
+    //song1 = loadSound('');
+    song1.setVolume(0.7);
+}
+
+function draw(){
+    background(0);
+    Image(img,1,1)
+}
