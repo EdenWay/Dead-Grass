07c06ad9cba498317bc58234e1eb05254d7550ad
EDEN <edenway@my.unt.edu>
2024-12-02 19:23:17 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 function setup() {
-    createCanvas(1920, 1920);
-  }
-  
-  function draw() {
-    background(164,160,133);
-  }
+    createCanvas(1920, 1080);
+    background(164, 160, 133);
+}
+
+function draw() {
+    fill(255);
+    ellipse(mouseX, mouseY, 50, 50);
+}
