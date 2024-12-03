578ee94dd8295fcaa5961abdb7a7b4aab7452a3f
dlj0224 <danielajimenez@my.unt.edu>
2024-12-03 17:51:15 -0600

    Update sketch3.js

--- sketch3.js
+++ sketch3.js
@@ -1,34 +1,44 @@
 let video;
 let keysPressed = { w: false, a: false, l: false, k: false };
+let darkness = 0; // Variable to control the darkness level
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Load video
+  // Load the video
   video = createVideo(['02.mp4']);
-  video.hide();  // Hide vid
-  video.loop();  // vid play
+  video.hide();  // Hide the default video player
+  video.loop();  // Make the video loop when played
 }
 
 function draw() {
   background(0);
 
+  // Check if all keys are pressed
   if (keysPressed.w && keysPressed.a && keysPressed.l && keysPressed.k) {
-    // Start playing the video when all keys are pressed 
+    // Start playing the video when all keys are pressed
     if (video.time() === 0) {
       video.play(); // Start playing the video when all keys are pressed
     }
-    // Display vid
-    image(video, 0, 0, width, height); 
+    // Display the vid
+    image(video, 0, 0, width, height);
   } else {
     // If the keys are not pressed
     textAlign(CENTER, CENTER);
     fill(255);
     textSize(32);
-    text('Press W, A, L, K', width / 2, height / 2);
+    text('Press W, A, L, K to start the video', width / 2, height / 2);
     textSize(14);
     text('Back', width / 30, height / 30);
   }
+
+  // Gradually dark
+  darkness = lerp(darkness, 255, 0.002); // Slowly increase the darkness (lerp to 255)
+
+  
+  fill(0, darkness);
+  noStroke();
+  rect(0, 0, width, height);
 }
 
 function keyPressed() {
