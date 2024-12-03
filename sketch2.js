ff5183064302b189a4b5bfdd9b317637b94fe807
EDEN <edenway@my.unt.edu>
2024-12-02 20:49:54 -0600

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -24,59 +24,52 @@ let currentLine = 0;
 let displayedText = "";
 let charIndex = 0;
 let typingSpeed = 4;
-let textColor;
+let textColor = [204, 163, 35];
 let showBackButton = false;
 
-let windSound, typingSound;
+let windSound;
+let typingSound;
 let isTyping = false;
 
 function preload() {
-  myGif = createImg("1.gif", ""); 
-  windSound = loadSound("wind.mp3");
-  typingSound = loadSound("typing_sound.mp3");
-  font = loadFont("Constantia.ttf"); 
+  myGif = loadImage("assets/1.gif");
+  font = loadFont("assets/Constantia.ttf");
+  windSound = loadSound("assets/wind.mp3");
+  typingSound = loadSound("assets/typing_sound.mp3");
 }
 
 function setup() {
   createCanvas(1920, 1080);
-  background(0);
-  textColor = color(204, 163, 35);
   textFont(font);
   textSize(48);
   fill(textColor);
   textAlign(CENTER, BOTTOM);
-
-  myGif.position(0, 0);
-  myGif.size(width, height);
-  myGif.style("z-index", "-1"); 
-  myGif.style("display", "block"); 
-
   windSound.loop();
 }
 
 function draw() {
-  background(0); 
-  image(myGif.elt, 0, 0, width, height); 
+  background(0);
+  image(myGif, 0, 0, width, height);
 
-  if (frameCount % typingSpeed === 0 && charIndex < textLines[currentLine].length) {
-    displayedText += textLines[currentLine].charAt(charIndex);
-    charIndex++;
-    if (!isTyping) {
-      typingSound.play();
-      isTyping = true;
+  if (!showBackButton) {
+    if (frameCount % typingSpeed === 0 && charIndex < textLines[currentLine].length) {
+      displayedText += textLines[currentLine][charIndex];
+      charIndex++;
+
+      if (!isTyping) {
+        typingSound.loop();
+        isTyping = true;
+      }
     }
-  }
 
-  if (charIndex >= textLines[currentLine].length && isTyping) {
-    typingSound.stop();
-    isTyping = false;
-    if (currentLine === textLines.length - 1) {
-      showBackButton = true;
+    if (charIndex >= textLines[currentLine].length && isTyping) {
+      typingSound.stop();
+      isTyping = false;
     }
-  }
 
-  fill(textColor);
-  text(displayedText, width / 2, height - 150);
+    fill(textColor);
+    text(displayedText, width / 2, height - 150);
+  }
 
   if (showBackButton) {
     textAlign(LEFT, TOP);
@@ -85,8 +78,8 @@ function draw() {
 }
 
 function mousePressed() {
-  if (showBackButton && mouseX > 50 && mouseX < 150 && mouseY > 50 && mouseY < 100) {
-    window.location.href = "index.html"; 
+  if (showBackButton && mouseX > 50 && mouseX < 200 && mouseY > 50 && mouseY < 100) {
+    window.location.href = "index.html";
   }
 
   if (charIndex >= textLines[currentLine].length) {
@@ -94,6 +87,9 @@ function mousePressed() {
       currentLine++;
       displayedText = "";
       charIndex = 0;
+    } else {
+      displayedText = "";
+      showBackButton = true;
     }
   }
 }
